8a45f964d40e56b3a77f508acfd4b9f87ef4a291
Fxbxzn <00194725@uca.edu.sv>
2025-05-13 10:39:35 -0600

    Ejercicio1

--- ejer1.cpp
+++ ejer1.cpp
@@ -27,6 +27,7 @@ int main(){
     
     }
     int n1;
+    int cartera=300;
     
     cout << ".\n que tipo de ropa necesitas.\n1 Ropa de hombre.\n2 ropa de mujer .\n3  Mostrar todo"<< endl;
     cin >> n1;
@@ -34,16 +35,89 @@ int main(){
     switch (n1)
     {
     case 1:
-        cout << "Camisa Casual: $90 (descuento del 30%)" << endl;
-        cout << "Pantalón Formal: $130 (descuento del 30%)" << endl;
-        cout << "Pantalón Formal: $130 (descuento del 30%)" << endl;
+
+    int ropah,ropam;
+    cout << " \n1 Camisa Casual: $90 (descuento del 30%).\n2 Pantalon formal: 130 (descuento del 30%).\n3 chaqueta premium: 900 (descuento del 30%) " << endl;
+    cin >> ropah;
+
+    switch (ropah)
+    {
+    case 1:
+    cout << "se agrega a tu cartera" << cartera - 90*0.3 << endl;
+        
         break;
         case 2:
+        cout << "se agrega a tu cartera" << cartera - 130*0.3 << endl;
+        break;
+        case 3:
+        cout << "se agrega a tu cartera" << cartera - 900*0.3 << endl;
+        break;
+    
+    default:
+    cout << " no es valido ingresa otro numero" << endl;
+        break;
+    }
+        cout << "Camisa Casual: $90 (descuento del 30%)" << endl;
+        cout << "Pantalón Formal: $130 (descuento del 30%)" << endl;
+        cout << "Abrigo premium: $250 (descuento del 30%)" << endl;
+        break;
+        case 2:
+        cout << " \n1 Blusa Moderna: $80 (descuento del 20%).\n2 Falda Elegante: $110 (descuento del 20%) .\n3 Abrigo Premium: $250 (descuento del 20%) " << endl;
+        cin >> ropam;
+
+        switch (ropam)
+        {
+        case 1:
+            cout << "se agrego a tu cartera "<< cartera - 80*0.2 << endl;
+            break;
+            case 2:
+            cout << "se agrego a tu cartera "<< cartera - 110*0.2 << endl;
+            break;
+            case 3:
+            cout << "se agrego a tu cartera" << cartera -250*0.2 << endl;
+            break;
+        default:
+        cout << "No es valido tu respuesta"<< endl;
+            break;
+        }
+
+
         cout << " Blusa Moderna: $80 (descuento del 20%)" << endl;
         cout << " Falda Elegante: $110 (descuento del 20%) "<< endl;
         cout << "Abrigo Premium: $250 (descuento del 20%)" << endl;
         break;
         case 3:
+        int todo;
+        cout << "\n1 Camisa Casual: $90 (descuento del 30%).\n2 Pantalon formal: 130 (descuento del 30%).\n3 chaqueta premium: 900 (descuento del 30%) \n4 Blusa Moderna: $80 (descuento del 20%).\n5 Falda Elegante: $110 (descuento del 20%) .\n6 Abrigo Premium: $250 (descuento del 20%)" << endl;
+        cin >> todo;
+
+        switch (todo)
+        {
+        case 1:
+            cout << "se agrega a tu cartera" << cartera - 90*0.3 << endl;
+        
+        break;
+        case 2:
+        cout << "se agrega a tu cartera" << cartera - 130*0.3 << endl;
+        break;
+        case 3:
+        cout << "se agrega a tu cartera" << cartera - 900*0.3 << endl;
+        break;
+        case 4:
+        cout << "se agrego a tu cartera "<< cartera - 80*0.2 << endl;
+        break;
+        case 5:
+        cout << "se agrego a tu cartera "<< cartera - 110*0.2 << endl;
+        break;
+        case 6:
+        cout << "se agrego a tu cartera" << cartera -250*0.2 << endl;
+        break;
+        
+        
+        default:
+        cout << "No es valido tu respuesta"<< endl;
+            break;
+        }
         cout << "Camisa Casual: $90 (descuento del 30%)" << endl;
         cout << "Pantalón Formal: $130 (descuento del 30%)" << endl;
         cout << "Pantalón Formal: $130 (descuento del 30%)" << endl;
@@ -55,7 +129,7 @@ int main(){
     
     
     }
-
+    
     
 
 
